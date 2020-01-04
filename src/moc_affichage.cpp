37a6b522f4872d8703ab4034fe78898db29dc57f
Denis Chheang <denis.chheang@ens.uvsq.fr>
2020-01-04 18:56:16 +0100

    merge

--- src/moc_affichage.cpp
+++ src/moc_affichage.cpp
@@ -522,15 +522,9 @@ int Valve::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     }
     return _id;
 }
-<<<<<<< HEAD
 struct qt_meta_stringdata_ValveTank_t {
     QByteArrayData data[3];
     char stringdata0[23];
-=======
-struct qt_meta_stringdata_MainWindow_t {
-    QByteArrayData data[8];
-    char stringdata0[84];
->>>>>>> Nico
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
@@ -539,28 +533,12 @@ struct qt_meta_stringdata_MainWindow_t {
     )
 static const qt_meta_stringdata_ValveTank_t qt_meta_stringdata_ValveTank = {
     {
-<<<<<<< HEAD
 QT_MOC_LITERAL(0, 0, 9), // "ValveTank"
 QT_MOC_LITERAL(1, 10, 11), // "clickedSlot"
 QT_MOC_LITERAL(2, 22, 0) // ""
 
     },
     "ValveTank\0clickedSlot\0"
-=======
-QT_MOC_LITERAL(0, 0, 10), // "MainWindow"
-QT_MOC_LITERAL(1, 11, 10), // "saveSignal"
-QT_MOC_LITERAL(2, 22, 0), // ""
-QT_MOC_LITERAL(3, 23, 10), // "loadSignal"
-QT_MOC_LITERAL(4, 34, 7), // "saveLog"
-QT_MOC_LITERAL(5, 42, 7), // "loadLog"
-QT_MOC_LITERAL(6, 50, 17), // "accountConnection"
-QT_MOC_LITERAL(7, 68, 15) // "accountCreation"
-
-    },
-    "MainWindow\0saveSignal\0\0loadSignal\0"
-    "saveLog\0loadLog\0accountConnection\0"
-    "accountCreation"
->>>>>>> Nico
 };
 #undef QT_MOC_LITERAL
 
@@ -570,16 +548,11 @@ static const uint qt_meta_data_ValveTank[] = {
        7,       // revision
        0,       // classname
        0,    0, // classinfo
-<<<<<<< HEAD
        1,   14, // methods
-=======
-       6,   14, // methods
->>>>>>> Nico
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
        0,       // flags
-<<<<<<< HEAD
        0,       // signalCount
 
  // slots: name, argc, parameters, tag, flags
@@ -587,29 +560,6 @@ static const uint qt_meta_data_ValveTank[] = {
 
  // slots: parameters
     QMetaType::Void,
-=======
-       2,       // signalCount
-
- // signals: name, argc, parameters, tag, flags
-       1,    1,   44,    2, 0x06 /* Public */,
-       3,    1,   47,    2, 0x06 /* Public */,
-
- // slots: name, argc, parameters, tag, flags
-       4,    0,   50,    2, 0x0a /* Public */,
-       5,    0,   51,    2, 0x0a /* Public */,
-       6,    0,   52,    2, 0x0a /* Public */,
-       7,    0,   53,    2, 0x0a /* Public */,
-
- // signals: parameters
-    QMetaType::Void, QMetaType::QString,    2,
-    QMetaType::Void, QMetaType::QString,    2,
-
- // slots: parameters
-    QMetaType::Void,
-    QMetaType::Void,
-    QMetaType::Void,
-    QMetaType::Void,
->>>>>>> Nico
 
        0        // eod
 };
@@ -620,16 +570,7 @@ void ValveTank::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, v
         ValveTank *_t = static_cast<ValveTank *>(_o);
         Q_UNUSED(_t)
         switch (_id) {
-<<<<<<< HEAD
         case 0: _t->clickedSlot(); break;
-=======
-        case 0: _t->saveSignal((*reinterpret_cast< QString(*)>(_a[1]))); break;
-        case 1: _t->loadSignal((*reinterpret_cast< QString(*)>(_a[1]))); break;
-        case 2: _t->saveLog(); break;
-        case 3: _t->loadLog(); break;
-        case 4: _t->accountConnection(); break;
-        case 5: _t->accountCreation(); break;
->>>>>>> Nico
         default: ;
         }
     }
@@ -661,7 +602,6 @@ int ValveTank::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-<<<<<<< HEAD
         if (_id < 1)
             qt_static_metacall(this, _c, _id, _a);
         _id -= 1;
@@ -669,15 +609,6 @@ int ValveTank::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
         if (_id < 1)
             *reinterpret_cast<int*>(_a[0]) = -1;
         _id -= 1;
-=======
-        if (_id < 6)
-            qt_static_metacall(this, _c, _id, _a);
-        _id -= 6;
-    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 6)
-            *reinterpret_cast<int*>(_a[0]) = -1;
-        _id -= 6;
->>>>>>> Nico
     }
     return _id;
 }
